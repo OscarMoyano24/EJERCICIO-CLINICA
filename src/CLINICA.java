6e7fe4911f25883bcd988ec246bc9716ae30e9d3
fanny gutierrez <omoyano@ucundinamarca.edu.co>
2026-03-02 21:31:14 -0500

    Primer commit

--- src/CLINICA.java
+++ src/CLINICA.java
@@ -1,4 +1,145 @@
-package PACKAGE_NAME;
-
 public class CLINICA {
+    // ATRIBUTOS
+
+    private String  nombre;
+    private int  edad;
+    private double peso;
+    private double presionArterial;
+    private boolean estable;
+
+    //CONSTRUCTOR VACIO//
+
+    public CLINICA() {
+    }
+    // CONSTRUCTOR CONN PARAMETROS//
+
+    public CLINICA(String nombre, int edad, double peso, double presionArterial, boolean estable) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.presionArterial = presionArterial;
+        this.estable = estable;
+    }
+
+    // GETTER-SETTER//
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getPresionArterial() {
+        return presionArterial;
+    }
+
+    public void setPresionArterial(double presionArterial) {
+        this.presionArterial = presionArterial;
+    }
+
+    public boolean isEstable() {
+        return estable;
+    }
+
+    public void setEstable(boolean estable) {
+        this.estable = estable;
+    }
+
+    //TOSTRING//
+
+
+    @Override
+    public String toString() {
+        return "CLINICA{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", peso=" + peso +
+                ", presionArterial=" + presionArterial +
+                ", estable=" + estable +
+                '}';
+    }
+
+
+    //METODO CUMPLIR AÑOS//
+    public int cumplirAños(){
+        this.edad +=1;
+        System.out.println(" la persona ha cumplido un año mas");
+        return this.edad;
+    }
+     //METODO AUMENTAR PESO//
+
+     public double aumentarPeso(double cantidad){
+        this.peso += cantidad;
+         System.out.println("la persona aumento de peso");
+         return this.peso;
+     }
+    //METODO DISMINUIR PESO//
+     public double disminuirPeso(double cantidad){
+        this.peso -=cantidad;
+         System.out.println("la persona disminuyo de peso");
+         return this.peso;
+     }
+
+     //METODO SUBIR PRESION//
+
+    public double  subirPresion(double valor){
+        this.presionArterial += valor;
+        System.out.println("la persona subio de presion arterial");
+        return this.presionArterial;
+    }
+
+    //METODO BAJAR PRESION//
+
+    public double bajarPresion(double valor){
+        this.presionArterial -= valor;
+        System.out.println("la persona tiene la presion arterial baja");
+        return this.presionArterial;
+    }
+
+    //METODO EMPEORAR ESTADO//
+
+    public void empeorarEstado(){
+        this.estable = false;
+
+        System.out.println(" l persona empeoro su estado");
+    }
+
+    //METODO MEJORAR ESTADO//
+
+    public void mejorarEstado(){
+        this.estable = true;
+        System.out.println("la persona mejoro su estado");
+    }
+
+    //METODO HISTORIAL CLINICO//
+
+    public void mostrarHistorialClinica(){
+        System.out.println(this.toString());
+    }
+
+
+
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+      Scanner teclado = new Scanner(System.in);
+      CLINICA paciente1 = new CLINICA();
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+      paciente1.setNombre("Andres");
+      paciente1.setEdad(28);
+      paciente1.setPeso(65);
+      paciente1.setPresionArterial(120);
+      paciente1.setEstable(false);
+
+      System.out.println(paciente1.toString());
+
+      int opcion;
+
+      do {
+            System.out.println("\n MENU ");
+            System.out.println("1. Crear paciente");
+            System.out.println("2. Buscar paciente");
+            System.out.println("3. Modificar edad");
+            System.out.println("4. Modificar peso");
+            System.out.println("5. Modificar presion");
+            System.out.println("6. Cambiar estado(estable/inestable");
+            System.out.print("Opción: ");
+            opcion = teclado.nextInt();
+      }
+
+
 }
